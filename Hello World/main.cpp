ac6f654ebfa11d3ec5382be1e9d8ef6e8aca8291
filovUD <160760207+filovUD@users.noreply.github.com>
2024-02-21 10:59:33 -0500

    Add files via upload

---
+++ Hello World/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+using namespace std;
+
+  cout << "Hello World!";
+	
+	return 0;
+}
