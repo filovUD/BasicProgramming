50194d55e30526042545475214260b85a6116aab
filovUD <160760207+filovUD@users.noreply.github.com>
2024-03-07 07:07:52 -0500

    Add files via upload

---
+++ loop/for/main3.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int i=0;
+	if (i<9){
+		i++;
+		cout<< "i es igual a "<< i << endl;
+	}
+	{
+		i++;
+		cout<< "i es igual a "<< i << endl;
+	}
+	{
+		i++;
+		cout<< "i es igual a"<< i << endl;
+	}
+	{
+		i++;
+		cout<< "i es igual a "<< i << endl;
+	}
+	{
+		i++;
+		cout<< "i es igual a "<< i << endl;
+	}
+	
+	{
+		i++;
+		cout<< "i es igual a "<< i << endl;
+	}
+	{
+		i++;
+		cout<< "i es igual a "<< i << endl;
+	}
+	{
+		i++;
+		cout<< "i es igual a "<< i << endl;
+	}
+	{
+		i++;
+		cout<< "i es igual a "<< i << endl;
+	}
+	{
+		i++;
+		cout<< "i es igual a "<< i << endl;
+	}
+	if (i=9);
+	cout<<"i llego a 9"<< endl;
+	
+	return 0;
+}
+
