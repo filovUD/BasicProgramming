f33e6304b122e7282983707c6258baaba8a85d91
filovUD <160760207+filovUD@users.noreply.github.com>
2024-03-17 23:59:41 -0500

    Add files via upload

---
+++ While/While 1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc,char** argv) {
+  int i = 0;
+  while (i < 5) {
+    cout << i << "\n";
+    i++;
+  }
+  return 0;
+}
---
+++ While/While 2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+ 
+int main (int argc,char** argv) {
+   int a = 10;
+   while( a < 20 ) {
+      cout << "value of a: " << a << endl;
+      a++;
+   }
+   return 0;
+}
